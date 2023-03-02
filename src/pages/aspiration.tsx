b59ba9589916f1c47e5a25ce97dc73706cea271c
hadzami10 <achmad.had2003@mail.ugm.ac.id>
2023-03-02 13:11:32 +0700

    add drop file

--- src/pages/aspiration.tsx
+++ src/pages/aspiration.tsx
@@ -1,5 +1,7 @@
 import Form from '@/components/aspiration/Form';
+import Layout from '@/components/aspiration/Layout';
 import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Aspiration() {
 	return (
@@ -7,9 +9,38 @@ export default function Aspiration() {
 			<Head>
 				<title>Aspiration</title>
 			</Head>
-			<main>
-				<Form />
-			</main>
+			<div className="relative h-full w-full overflow-hidden rounded-xl bg-cover">
+				<Image
+					src="/images/bg/hero.png"
+					fill
+					alt="hero"
+					style={{
+						zIndex: -1,
+					}}
+				/>
+				<div className="grid grid-cols-7 items-center gap-x-14 px-16 h-[600px]">
+					<div className="customMd:col-span-3 col-span-7 flex flex-col gap-y-8 text-white">
+						<h1>
+							<span className="text-6xl font-semibold">Aspirations</span>{' '}
+							<span className="text-5xl font-medium">for the future.</span>
+						</h1>
+						<p className="text-justify">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+							turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+							nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+							tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+							feugiat lectus. Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed
+							dignissim, metus nec fringilla accumsan, risus sem sollicitudin
+							lacus, ut interdum tellus elit sed risus. Maecenas eget
+							condimentum velit, sit amet feugiat lectus.{' '}
+						</p>
+					</div>
+					<div className="col-span-7 customMd:col-span-4">
+						<Layout />
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
