5b71dd98eb588157ed6c42fc2dfda0c5d0365440
kadek <kadeknindap@gmail.com>
2023-03-04 16:45:17 +0700

    commit tailwind.config

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -3,7 +3,7 @@ import { Lato } from 'next/font/google';
 
 export default function Layout(props: { children: React.ReactNode }) {
 	return (
-		<div className='font-Lato'>
+		<div className='font-Lato	'>
 			<Header />
 			<div className="layout__content">{props.children}</div>
 		</div>
