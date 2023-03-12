7f6434287bc8018ec0cc00f3d9762ff2aedfc6d1
kadek <kadeknindap@gmail.com>
2023-03-12 20:16:18 +0700

    update event

--- src/pages/event.tsx
+++ src/pages/event.tsx
@@ -1,9 +1,8 @@
 import Head from "next/head";
 import Image from "next/image"
 import FotoAngkatan from "public/images/bg/foto_angkatan.png"
-import TextureEvent from "public/images/bg/texture_page_events.png"
 import Pattern from "public/images/bg/patternpad.png"
-import DropDown from "public/images/icon/dropDown.svg";
+import img_Dropdown from "public/images/icon/dropDown.svg";
 import FindArticle from "src/components/detail-post/Searchbutton"
 import img_Mikat from "public/images/bg/Image_Mikat.png"
 import img_KPM from "public/images/bg/image_KPM.png"
@@ -11,16 +10,20 @@ import img_Hublu from "public/images/bg/image_Hublu.png"
 import img_PO from "public/images/bg/image_PO.png"
 import type { StaticImageData } from 'next/image'
 import React from "react";
+import Link from "next/link"
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment, useEffect, useRef, useState } from 'react'
 
 interface GridComponentProps {
     image: string | StaticImageData;
     divisi: string;
+    content: string;
 
 }
 
-const GridComponent = ({ image, divisi }: GridComponentProps) => {
+const GridComponent = ({ image, divisi, content }: GridComponentProps) => {
     return (
-        <a href="" >
+        <Link href={content} >
             <div className="flex w-[264px] h-[139px] bg-[#FFFFFF] rounded-[10px] md:h-[386px] md:w-[285px]">
                 <div className="flex flex-row md:flex-col">
                     <div className="w-full justify-center items-center">
@@ -49,14 +52,19 @@ const GridComponent = ({ image, divisi }: GridComponentProps) => {
                     </div>
                 </div>
             </div>
-        </a >
+        </Link >
     )
 };
 
-// const [isOpen, setIsOpen] = useState(false);
-// return (
+const DropDown = () => {
+    return (
+        <button className=" flex w-[99px] h-[33.71px] bg-[#F3BE00] rounded-[10px] left-1/2">
+            2023
+        </button>
 
-// )
+        
+    )
+};
 
 export default function Event() {
     return (
@@ -65,13 +73,13 @@ export default function Event() {
                 <title>Event</title>
             </Head>
             <main>
-                <div className="w-full flex-wrap relative">
+                <div className="w-full flex-wrap relative pt-10">
                     <Image
-                        src={'images/bg/patternpad.png'}
+                        src={Pattern}
                         quality={100}
                         alt="hero"
                         fill
-                        className="-z-[9999] object-cover"
+                        className="-z-[99] object-cover"
                     />
                     <div className="w-full h-screen flex flex-col justify-center items-center content-around gap-[15px] p-10">
                         <div className="w-full h-full flex bg-[#FFFFFF] rounded-[20px] justify-center md:p-20">
@@ -100,75 +108,77 @@ export default function Event() {
                         </div>
                     </div>
                 </div>
-                <div>
-                    <div className="w-full h-[172px] md:h-[275px] flex flex-col bg-[#F3BE00] justify-center items-center text-center ">
-                        <h3><span className="font-bold text-[40px] text-[#212152] md:text-[64px]">Archived</span></h3>
-                        <p className="w-[278px] md:w-[592px]"><span className="font-medium text-[15px] md:text-[20px] text-[#3F3F9C]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, </span></p>
+                <div className="relative w-full h-[172px] md:h-[275px] flex flex-col bg-[#F3BE00] justify-center items-center text-center ">
+                    <h3><span className="font-bold text-[40px] text-[#212152] md:text-[64px]">Archived</span></h3>
+                    <p className="w-[278px] md:w-[592px]"><span className="font-medium text-[15px] md:text-[20px] text-[#3F3F9C]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, </span></p>
+                    <div className="absolute flex flex-row justify-center items-center bottom-0 translate-y-1/2 z-10">
+                        <FindArticle />
                     </div>
-                    <div className="relative w-ful h-[781px] items-center justify-center">
-                        <Image
-                            src={FotoAngkatan}
-                            quality={100}
-                            alt=""
-                            fill
-                            className="relative h-[781px] object-cover opacity-60"
-                        />
-                        <div className="absolute flex flex-row justify-center items-center left-1/2 -translate-x-1/2 -top-5">
-                            <FindArticle />
-                        </div>
-                        <div className="absolute w-full p-14">
-                            <div className="flex w-[12/13]  mx-auto h-[50.75px] md:h-[100px] bg-[#212152] rounded-t-[12px] justify-end items-center">
-                                <div className="absolute -translate-x-3">
-                                    <button className="hidden md:flex flex-row h-[65px] w-[137px] bg-[#F3BE00] left-1/2 rounded-[19px]">
-                                        <div className="text-[#FFFFFF] text-[46px] font-bold translate-x-5">2023</div>
-                                        <Image src={DropDown} alt="" className="hidden md:absolute right-0 -translate-x-3 top-1/2 -translate-y-1" />
-                                    </button>
-                                    <button className="md:hidden flex w-[99px] h-[33.71px] bg-[#F3BE00] rounded-[10px] left-1/2">
-                                        <div className="text-[#FFFFFF] text-[20px] font-bold translate-x-5 ">2023</div>
-                                        <Image src={DropDown} alt="" className="md:hidden absolute right-0 -translate-x-3 top-1/2 -translate-y-1" />
-                                    </button>
-                                    <div className="md:hidden absolute w-[99px] h-[208px] bg-[#F3BE00] rounded-[10px] right-0 py-4">
-                                        <div className="flex flex-col overflow-scroll h-[208px] justify-center items-cente gap-2">
-                                            <a href="" className="flex w-full text-[20px] font-bold text-[#FFFFFF] hover:bg-[#212152] justify-center items-center">2017</a>
-                                            <a href="" className="flex w-full text-[20px] font-bold text-[#FFFFFF] hover:bg-[#212152] justify-center items-center">2018</a>
-                                            <a href="" className="flex w-full text-[20px] font-bold text-[#FFFFFF] hover:bg-[#212152] justify-center items-center">2019</a>
-                                            <a href="" className="flex w-full text-[20px] font-bold text-[#FFFFFF] hover:bg-[#212152] justify-center items-center">2020</a>
-                                            <a href="" className="flex w-full text-[20px] font-bold text-[#FFFFFF] hover:bg-[#212152] justify-center items-center">2021</a>
-                                            <a href="" className="flex w-full text-[20px] font-bold text-[#FFFFFF] hover:bg-[#212152] justify-center items-center">2022</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="w-[12/13] h-[651px] md:h-[580px] bg-white rounded-b-[12px] bg-blurBackground">
-                                <div className="flex flex-wrap gap-[20px] h-[651px] md:h-[580px] p-2 pt-5 justify-center overflow-auto md:justify-between md:px-12">
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                    <GridComponent image={img_Hublu} divisi={"HUBLU"} />
-                                    <GridComponent image={img_KPM} divisi={"KPM"} />
-                                    <GridComponent image={img_PO} divisi={"PO"} />
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                    <GridComponent image={img_Mikat} divisi={"MIKAT"} />
-                                </div>
-
-                            </div>
+                </div>
+                <div className="absolute w-full p-14">
+                    <div className="flex w-[12/13]  mx-auto h-[50.75px] md:h-[100px] bg-[#212152] rounded-t-[12px] justify-end items-center">
+                    <DropDown/>
+                    </div>
+                    <div className="w-[12/13] h-[651px] md:h-[580px] bg-white rounded-b-[12px] bg-blurBackground">
+                        <div className="flex flex-wrap gap-[20px] h-[651px] md:h-[580px] p-2 pt-5 justify-center overflow-auto md:justify-between md:px-12">
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
+                            <GridComponent image={img_Hublu} divisi={"HUBLU"} content="" />
+                            <GridComponent image={img_KPM} divisi={"KPM"} content="" />
+                            <GridComponent image={img_PO} divisi={"PO"} content="" />
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
+                            <GridComponent image={img_Mikat} divisi={"MIKAT"} content="" />
                         </div>
                     </div>
                 </div>
+                <div className="relative w-full h-[781px] items-center justify-center -z-10">
+                    <Image
+                        src={FotoAngkatan}
+                        quality={100}
+                        alt=""
+                        fill
+                        className="relative h-[781px] object-cover opacity-60"
+                    />
+                </div>
+                {/* <div className="absolute w-full p-14">
+                    <div className="flex w-[12/13]  mx-auto h-[50.75px] md:h-[100px] bg-[#212152] rounded-t-[12px] justify-end items-center">
+                        <div className="absolute z-[100] -translate-x-3">
+                            <button className="hidden md:flex flex-row h-[65px] w-[137px] bg-[#F3BE00] left-1/2 rounded-[19px]">
+                                <div className="text-[#FFFFFF] text-[46px] font-bold translate-x-5">2023</div>
+                                <Image src={DropDown} alt="" className="hidden md:absolute right-0 -translate-x-3 top-1/2 -translate-y-1" />
+                            </button>
+                            <button className="md:hidden flex w-[99px] h-[33.71px] bg-[#F3BE00] rounded-[10px] left-1/2">
+                                <div className="text-[#FFFFFF] text-[20px] font-bold translate-x-5 ">2023</div>
+                                <Image src={DropDown} alt="" className="md:hidden absolute right-0 -translate-x-3 top-1/2 -translate-y-1" />
+                            </button>
+                            <div className="md:hidden absolute w-[99px] h-[208px] bg-[#F3BE00] rounded-[10px] right-0 py-4">
+                                <div className="flex flex-col overflow-scroll h-[208px] justify-center items-cente gap-2">
+                                    <a href="" className="flex w-full text-[20px] font-bold text-[#FFFFFF] hover:bg-[#212152] justify-center items-center">2017</a>
+                                    <a href="" className="flex w-full text-[20px] font-bold text-[#FFFFFF] hover:bg-[#212152] justify-center items-center">2018</a>
+                                    <a href="" className="flex w-full text-[20px] font-bold text-[#FFFFFF] hover:bg-[#212152] justify-center items-center">2019</a>
+                                    <a href="" className="flex w-full text-[20px] font-bold text-[#FFFFFF] hover:bg-[#212152] justify-center items-center">2020</a>
+                                    <a href="" className="flex w-full text-[20px] font-bold text-[#FFFFFF] hover:bg-[#212152] justify-center items-center">2021</a>
+                                    <a href="" className="flex w-full text-[20px] font-bold text-[#FFFFFF] hover:bg-[#212152] justify-center items-center">2022</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
+
             </main>
         </>
     );
 }
-
